f039defcfe72af75994fc01f8f14f847ca32419e
Paweł <pawelkrystkiewicz@outlook.com>
2024-04-10 19:52:55 +0200

    feat: add build:watch to cui

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -4,7 +4,7 @@ const { withTailwindConfig } = require('@creation-ui/core')
 const config = withTailwindConfig({
   content: [
     //
-    './packages/**/*.{js,ts,jsx,tsx,mdx}',
+    '../packages/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     '!node_modules',
