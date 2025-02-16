9da3a9296ef50f625ac8eb0abb354acc94d907bd
Paweł <pawelkrystkiewicz@outlook.com>
2025-02-16 19:55:16 +0100

    feat: drawer, custom main colors

---
+++ apps/docs/src/examples/drawer.tsx
@@ -0,0 +1,143 @@
+'use client'
+import { Container } from '@/components/container'
+import { Playground } from '@/components/playground'
+import type { DocumentedProperty } from '@/models/system'
+import { Button, Drawer, type DrawerProps } from '@creation-ui/react'
+import { useState } from 'react'
+import { positionControl } from './shared-playground-controls'
+import { childrenProp, openProp, positionProp } from './shared-props'
+
+export const DrawerExample = (props: DrawerProps) => {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  return (
+    <>
+      <Button onClick={handleOpen}>Open Drawer</Button>
+      <Drawer open={open} onClose={handleClose} {...props}>
+        <div className='p-5'>
+          <h1>Payment successful</h1>
+          <div className='mt-2'>
+            <p className='text-sm text-gray-500'>
+              Your payment has been successfully submitted. We&apos;ve sent you
+              an email with all of the details of your order.
+            </p>
+          </div>
+          <div className='mt-4'>
+            <Button onClick={handleClose}>Got it, thanks!</Button>
+          </div>
+        </div>
+      </Drawer>
+    </>
+  )
+}
+
+export const DrawerPlayground = () => (
+  <Playground
+    component={DrawerExample}
+    controls={[positionControl]}
+    code={`export const DrawerExample = (props: DrawerProps) => {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  return (
+    <>
+      <Button onClick={handleOpen}>Open Drawer</Button>
+      <Drawer open={open} onClose={handleClose} {{props}}>
+        <div className='p-5'>
+          <h1>Payment successful</h1>
+          <div className='mt-2'>
+            <p className='text-sm text-gray-500'>
+              Your payment has been successfully submitted. We&apos;ve sent you
+              an email with all of the details of your order.
+            </p>
+          </div>
+          <div className='mt-4'>
+            <Button onClick={handleClose}>Got it, thanks!</Button>
+          </div>
+        </div>
+      </Drawer>
+    </>
+  )
+}`}
+  />
+)
+
+export const DrawerCustomizedExample = (props: DrawerProps) => {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  return (
+    <Container>
+      <Button onClick={handleOpen}>Open Drawer</Button>
+      <Drawer
+        {...props}
+        open={open}
+        onClose={handleClose}
+        cx={{
+          width: 'xl:w-1/3 lg:w-1/2 w-full',
+          container: {
+            outer: 'p-[15px]',
+            inner: 'rounded-xl',
+          },
+        }}
+      >
+        <div className='p-5 flex flex-col h-full gap-2'>
+          <h1>Title</h1>
+          <div className='flex-grow'>
+            <p className='text-sm text-gray-500'>Content</p>
+          </div>
+          <div className='flex gap-2 items-center'>
+            <Button onClick={handleClose} className='min-w-[100px]'>
+              Save
+            </Button>
+            <Button
+              onClick={handleClose}
+              variant={'outlined'}
+              color={'error'}
+              className='min-w-[100px]'
+            >
+              Cancel
+            </Button>
+          </div>
+        </div>
+      </Drawer>
+    </Container>
+  )
+}
+
+export const properties: DocumentedProperty[] = [
+  openProp,
+  childrenProp,
+  positionProp,
+  {
+    description: 'Callback function called when closing Drawer',
+    name: 'onClose',
+    type: '() => void',
+  },
+  {
+    description:
+      'Callback function called on overlay click. Maybe used to close Drawer or prevent closing.',
+    name: 'onOverlayClick',
+    type: '() => void',
+  },
+  {
+    name: 'cx',
+    description:
+      'Class names manipulation API. Width and height control how much space Drawer takes. <code>container.outer</code> and <code>container.inner</code> control can be used to change how content is styled.',
+    type: JSON.stringify({
+      'height?': 'string',
+      'width?': 'string',
+      'container?': {
+        'outer?': 'string',
+        'inner?': 'string',
+      },
+    }),
+  },
+]
---
+++ packages/ui/src/drawer/Drawer.tsx
@@ -0,0 +1,60 @@
+import { Dialog, Transition } from '@headlessui/react'
+import clsx from 'clsx'
+import { Fragment } from 'react'
+import { twMerge } from 'tailwind-merge'
+import { Overlay } from '../overlay'
+import { drawerAnimation, drawerChildClasses, drawerStyles } from './classes'
+import type { DrawerProps } from './types'
+
+export const Drawer = ({
+  open,
+  children,
+  onOverlayClick,
+  ...props
+}: DrawerProps) => {
+  const { position = 'right', onClose = () => {}, cx } = props
+
+  const verticalSize = cx?.height ?? 'h-full'
+  const horizontalSize = cx?.width ?? 'w-1/3'
+
+  const finalSize = {
+    right: horizontalSize,
+    left: horizontalSize,
+    top: verticalSize,
+    bottom: verticalSize,
+  }[position]
+
+  return (
+    <>
+      <Overlay className={'!fixed'} active={open} onClick={onOverlayClick} />
+      <Transition
+        show={open}
+        as={Fragment}
+        unmount={false}
+        enter={clsx(drawerAnimation.animation)}
+        leave={clsx(drawerAnimation.animation)}
+        enterFrom={clsx(drawerAnimation.enter[position])}
+        enterTo={clsx(drawerAnimation.leave[position])}
+        leaveFrom={clsx(drawerAnimation.leave[position])}
+        leaveTo={clsx(drawerAnimation.enter[position])}
+      >
+        <Dialog
+          unmount={false}
+          onClose={onClose}
+          className={twMerge(
+            drawerStyles({
+              className: [finalSize, cx?.container?.outer],
+              position,
+            }),
+          )}
+        >
+          <div
+            className={clsx(drawerChildClasses, 'h-full', cx?.container?.inner)}
+          >
+            {children}
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
