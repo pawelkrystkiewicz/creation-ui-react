129b3a6b571cc961b5f06b8b33a3e067f752b2ed
Paweł <pawelkrystkiewicz@outlook.com>
2025-04-09 11:27:23 +0200

    test: beautify snapshots directories

--- packages/ui/playwright-ct.config.ts
+++ packages/ui/playwright-ct.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig, devices } from '@playwright/experimental-ct-react'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const __dirname = path.resolve()
 
 export default defineConfig({
   testMatch: '**/*.ct.spec.tsx',
+  testDir: path.join(__dirname, 'src'),
   outputDir: './playwright/output',
   timeout: 10 * 1000,
+  snapshotPathTemplate:
+    '{testDir}/__screenshots__{/projectName}/{testFilePath}/{arg}{ext}',
+
+  reporter: process.env.CI ? 'github' : 'html',
   use: {
     ctViteConfig: {
       plugins: [react(), tailwindcss()],
