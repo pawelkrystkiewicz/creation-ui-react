f2c4d31f1e64fcbb2c6aa6d721116787063f57a7
Paweł <pawelkrystkiewicz@outlook.com>
2025-04-09 15:56:26 +0200

    test: update button states tests

--- packages/ui/src/button/__specs__/button.ct.spec.tsx
+++ packages/ui/src/button/__specs__/button.ct.spec.tsx
@@ -139,25 +139,45 @@ test.describe('Button CT', () => {
       normal: {
         props: {},
         expected: {
-          css: [],
+          css: [
+            ['color', 'oklch(0.6048 0.2165 257.21)'],
+            ['background-color', 'oklab(0.6048 -0.0479284 -0.211128 / 0.1)'],
+            ['transform', 'none'],
+            ['opacity', '1'],
+          ],
         },
       },
       active: {
         props: {},
         expected: {
-          css: [],
+          css: [
+            ['color', 'oklch(0.6048 0.2165 257.21)'],
+            ['background-color', 'oklab(0.6048 -0.0479284 -0.211128 / 0.1)'],
+            ['transform', 'none'],
+            ['opacity', '1'],
+          ],
         },
       },
       hover: {
         props: {},
         expected: {
-          css: [],
+          css: [
+            ['color', 'oklch(0.6048 0.2165 257.21)'],
+            ['background-color', 'oklab(0.6048 -0.0479284 -0.211128 / 0.1)'],
+            ['transform', 'none'],
+            ['opacity', '1'],
+          ],
         },
       },
       focus: {
         props: {},
         expected: {
-          css: [],
+          css: [
+            ['color', 'oklch(0.6048 0.2165 257.21)'],
+            ['background-color', 'oklab(0.6048 -0.0479284 -0.211128 / 0.1)'],
+            ['transform', 'none'],
+            ['opacity', '1'],
+          ],
         },
       },
     }
@@ -230,6 +250,8 @@ test.describe('Button CT', () => {
 
         // Cleanup
         await page.mouse.up()
+        // loose focus
+        await button.blur()
 
         for (const [property, value] of scenarios.normal.expected.css) {
           await expect(button).toHaveCSS(property, value)
