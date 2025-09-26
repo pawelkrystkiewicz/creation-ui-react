e4378e1174f1fe466d02ea91b8aa21bd1c7679aa
Paweł <pawelkrystkiewicz@outlook.com>
2025-09-26 11:07:05 +0200

    fix: label text color

--- packages/ui/src/label/classes.ts
+++ packages/ui/src/label/classes.ts
@@ -3,11 +3,10 @@ import { cva } from 'class-variance-authority'
 export const labelStyles = cva(
   [
     'text-base/6',
-    'text-neutral-950',
+    'text-text-primary',
     'select-none',
     'data-disabled:opacity-50',
     'sm:text-sm/6',
-    'dark:text-white',
   ],
   {
     variants: {
