b9ed3313e1c92e0fc440bfbac4277876d085d23d
Paweł <pawelkrystkiewicz@outlook.com>
2025-11-28 12:28:23 +0100

    ``chore: update bun.lock and package.json for dependency management
    - Changed project name in bun.lock from 'design-system' to 'creation-ui-react'
    - Added 'trustedDependencies' section in package.json for core-js-pure and unrs-resolver
    - Updated various package versions in bun.lock for improved compatibility and performance
    - Modified 'nuke' script in package.json to include '--ignore-scripts' for better cleanup

--- packages/ui/src/overlay/Overlay.tsx
+++ packages/ui/src/overlay/Overlay.tsx
@@ -1,11 +1,8 @@
 import { Transition } from '@headlessui/react'
-import { Activity, type FC, Fragment, version as reactVersion } from 'react'
+import { Activity, type FC, Fragment } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { overlay } from './classes'
 import type { OverlayProps } from './types'
-
-console.log('Activity', typeof Activity) //Activity undefined
-console.log('React version', reactVersion) //React version 19.2.0-canary-0bdb9206-20250818
 
 export const Overlay: FC<OverlayProps> = ({
   active,
@@ -15,24 +12,24 @@ export const Overlay: FC<OverlayProps> = ({
   children,
 }) => {
   return (
-    <Activity mode={active ? 'visible' : 'hidden'}>
-      <Transition
-        show={!!active}
-        as={Fragment}
-        enter={'micro-interactions'}
-        leave={'micro-interactions'}
-        enterTo='opacity-100'
-        enterFrom='opacity-0'
-        leaveFrom='opacity-100'
-        leaveTo='opacity-0'
-      >
+    <Transition
+      show={!!active}
+      as={Fragment}
+      enter={'micro-interactions'}
+      leave={'micro-interactions'}
+      enterTo='opacity-100'
+      enterFrom='opacity-0'
+      leaveFrom='opacity-100'
+      leaveTo='opacity-0'
+    >
+      <Activity mode={active ? 'visible' : 'hidden'}>
         <div
           className={twMerge(overlay({ cursorWait }), className)}
           onClick={onClick}
         >
           {children}
         </div>
-      </Transition>
-    </Activity>
+      </Activity>
+    </Transition>
   )
 }
